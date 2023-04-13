742d19edeeaca6d1d9619dd99531f4e3c93dfffb
Tyler Laceby <31893030+tylerbadger23@users.noreply.github.com>
2023-04-13 09:59:50 -0600

    file rework

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,11 @@
-#include "util/cli.h"
+
 #include <iostream>
 #include <unistd.h>
 
 //
 
 int main(int argc, char **argv) {
-  em::cli::args();
-  int opt;
-  while ((opt = getopt(argc, argv, "tp")) != -1) {
-    switch (opt) {
-    case 't':
-      std::cout << "Time flag is set" << std::endl;
-      break;
-    case 'p':
-      std::cout << "Prod flag is set" << std::endl;
-      break;
-    default:
-      std::cerr << "Usage: " << argv[0] << " [-t] [-p]" << std::endl;
-      return 1;
-    }
-  }
-
-  // Your code here
+  printf("\n");
 
   return 0;
 }
